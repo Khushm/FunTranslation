9cdbcd2a1f44a17d5958550cfc661c9f9694bd9f
suhas-kadu <kadusuhas72@gmail.com>
2021-06-27 12:16:11 +0530

    api integrated

---
+++ js/app.js
@@ -0,0 +1,34 @@
+  var translateButton = document.querySelector("#translate-btn")
+var inputText = document.querySelector(".text-input")
+var outputText = document.querySelector(".text-output")
+
+var url = "https://api.funtranslations.com/translate/minion.json"
+
+const getTranslatedUrl = (input) => {
+    return url + "?" + "text=" + input;
+}
+
+const catchError = (e) => {
+    console.log(e);
+    alert("Something went wrong with the server");
+}
+
+const clickHandler = () => {
+    var txtInput = inputText.value;
+    console.log(txtInput);
+    
+    fetch(getTranslatedUrl(txtInput)).then(response => response.json()).then(json => {
+        var translatedText = json.contents.translated;
+        console.log(translatedText);
+        
+        outputText.innerHTML = translatedText
+    
+    }).catch(catchError)
+
+
+    
+
+}
+
+translateButton.addEventListener("click", clickHandler);
+
